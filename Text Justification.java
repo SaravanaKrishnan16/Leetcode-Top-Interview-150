11b6a03d896c37dca49942e640346a44648f5780
Saravana Krishnan J <168951123+SaravanaKrishnan16@users.noreply.github.com>
2025-08-08 07:34:08 +0530

    Strings

---
+++ Text Justification.java
@@ -0,0 +1,104 @@
+Given an array of strings words and a width maxWidth, format the text such that each line has exactly maxWidth characters and is fully (left and right) justified.
+
+You should pack your words in a greedy approach; that is, pack as many words as you can in each line. Pad extra spaces ' ' when necessary so that each line has exactly maxWidth characters.
+
+Extra spaces between words should be distributed as evenly as possible. If the number of spaces on a line does not divide evenly between words, the empty slots on the left will be assigned more spaces than the slots on the right.
+
+For the last line of text, it should be left-justified, and no extra space is inserted between words.
+
+Note:
+
+A word is defined as a character sequence consisting of non-space characters only.
+Each word's length is guaranteed to be greater than 0 and not exceed maxWidth.
+The input array words contains at least one word.
+ 
+
+Example 1:
+
+Input: words = ["This", "is", "an", "example", "of", "text", "justification."], maxWidth = 16
+Output:
+[
+   "This    is    an",
+   "example  of text",
+   "justification.  "
+]
+
+Example 2:
+
+Input: words = ["What","must","be","acknowledgment","shall","be"], maxWidth = 16
+Output:
+[
+  "What   must   be",
+  "acknowledgment  ",
+  "shall be        "
+]
+Explanation: Note that the last line is "shall be    " instead of "shall     be", because the last line must be left-justified instead of fully-justified.
+Note that the second line is also left-justified because it contains only one word.
+
+Example 3:
+
+Input: words = ["Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do"], maxWidth = 20
+Output:
+[
+  "Science  is  what we",
+  "understand      well",
+  "enough to explain to",
+  "a  computer.  Art is",
+  "everything  else  we",
+  "do                  "
+]
+
+Constraints:
+
+1 <= words.length <= 300
+1 <= words[i].length <= 20
+words[i] consists of only English letters and symbols.
+1 <= maxWidth <= 100
+words[i].length <= maxWidth
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class Solution {
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> result = new ArrayList<>();
+        int index = 0;
+
+        while (index < words.length) {
+            int totalChars = words[index].length();
+            int last = index + 1;
+
+            while (last < words.length) {
+                if (totalChars + 1 + words[last].length() > maxWidth) break;
+                totalChars += 1 + words[last].length();
+                last++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            int numOfWords = last - index;
+            if (last == words.length || numOfWords == 1) {
+                for (int i = index; i < last; i++) {
+                    sb.append(words[i]);
+                    if (i < last - 1) sb.append(" ");
+                }
+                int remaining = maxWidth - sb.length();
+                while (remaining-- > 0) sb.append(" ");
+            } else {
+                int totalSpaces = maxWidth - totalChars + (numOfWords - 1);
+                int spaceBetweenWords = totalSpaces / (numOfWords - 1);
+                int extraSpaces = totalSpaces % (numOfWords - 1);
+
+                for (int i = index; i < last; i++) {
+                    sb.append(words[i]);
+                    if (i < last - 1) {
+                        for (int s = 0; s < spaceBetweenWords; s++) sb.append(" ");
+                        if (extraSpaces-- > 0) sb.append(" ");
+                    }
+                }
+            }
+            result.add(sb.toString());
+            index = last;
+        }
+
+        return result;
+    }
+}

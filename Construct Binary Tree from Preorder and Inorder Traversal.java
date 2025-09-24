b843268987fa55423787fd85e3b5ca393c505882
Saravana Krishnan J <168951123+SaravanaKrishnan16@users.noreply.github.com>
2025-09-24 07:34:28 +0530

    Binary Tree

---
+++ Construct Binary Tree from Preorder and Inorder Traversal.java
@@ -0,0 +1,45 @@
+Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+Example 1:
+
+Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+Output: [3,9,20,null,null,15,7]
+
+Example 2:
+
+Input: preorder = [-1], inorder = [-1]
+Output: [-1]
+ 
+Constraints:
+
+1 <= preorder.length <= 3000
+inorder.length == preorder.length
+-3000 <= preorder[i], inorder[i] <= 3000
+preorder and inorder consist of unique values.
+Each value of inorder also appears in preorder.
+preorder is guaranteed to be the preorder traversal of the tree.
+inorder is guaranteed to be the inorder traversal of the tree.
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class Solution {
+    private int preIndex = 0;
+    private Map<Integer, Integer> inorderMap = new HashMap<>();
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for (int i = 0; i < inorder.length; i++) {
+            inorderMap.put(inorder[i], i);
+        }
+        return build(preorder, 0, inorder.length - 1);
+    }
+
+    private TreeNode build(int[] preorder, int left, int right) {
+        if (left > right) return null;
+        int rootVal = preorder[preIndex++];
+        TreeNode root = new TreeNode(rootVal);
+        int inIndex = inorderMap.get(rootVal);
+        root.left = build(preorder, left, inIndex - 1);
+        root.right = build(preorder, inIndex + 1, right);
+        return root;
+    }
+}
